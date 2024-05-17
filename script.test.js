0f24c7bb74f64391c3ba35572c639cd594616ea7
XinoXz-12 <jaimelozanolozano12@gmail.com>
2024-05-17 20:30:08 +0200

    CambioNombres

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-const mathOperations = require('./calculator');
+const mathOperations = require('./script.js');
  
 describe("Calculator tests", () => {
  test('adding 1 + 2 should return 3', () => {
