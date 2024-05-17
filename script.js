12aa06585833b26ae755869ec49cb5d98bb7d3c5
IsmaCar <carballomartinismael@gmail.com>
2024-05-17 20:23:22 +0200

    Update script

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-var win = null;
+const mathOperations = {
+  sum: function (a, b) {
+    return a + b;
+  },
 
-function NewWindow(mypage, myname, w, h, scroll, pos) {
-    if (pos == "random") {
-        LeftPosition = (screen.width) ? Math.floor(Math.random() * (screen.width - w)) : 100;
-        TopPosition = (screen.height) ? Math.floor(Math.random() * ((screen.height - h) - 75)) : 100;
-    }
-    if (pos == "center") {
-        LeftPosition = (screen.width) ? (screen.width - w) / 2 : 100;
-        TopPosition = (screen.height) ? (screen.height - h) / 2 : 100;
-    } else if ((pos != "center" && pos != "random") || pos == null) {
-        LeftPosition = 0;
-        TopPosition = 20
-    }
-    settings = 'width=' + w + ',height=' + h + ',top=' + TopPosition + ',left=' + LeftPosition + ',scrollbars=' + scroll + ',location=no,directories=no,status=no,menubar=no,toolbar=no,resizable=no';
-
-    win = window.open(mypage, myname, settings);
-}
+  diff: function (a, b) {
+    return a - b;
+  },
+  product: function (a, b) {
+    return a * b;
+  },
+};
+module.exports = mathOperations;
